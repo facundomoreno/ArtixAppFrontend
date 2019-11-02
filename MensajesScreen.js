7f1a5cdc52586005bfc777f746ad59c055234d66
jottod <jose@dolina.com>
2019-11-01 23:15:25 -0300

    pantalla mensajes

--- MensajesScreen.js
+++ MensajesScreen.js
@@ -18,86 +18,95 @@ import { createAppContainer } from "react-navigation";
 import CardMens from "./components/CardMens";
 
 datamens = [
-    {
-        key: 0,
-        image: require("./assets/shops/boca.jpeg"),
-        name: "Agustina Cabo",
-        mens: "¿Sabías que le re doy a Mastro?",
-        time: "8 hs."
-    },
-    {
-        key: 1,
-        image:require("./assets/sxhb.png"),
-        name: "Agustina Cabo",
-        mens: "¿Sabías que le re doy a Mastro?",
-        time: "8 hs."
-    },
-    {
-        key: 2,
-        image:require("./assets/sxhb.png"),
-        name: "Agustina Cabo",
-        mens: "¿Sabías que le re doy a Mastro?",
-        time: "8 hs."
-    }
+  {
+    key: 0,
+    image: require("./assets/avatar/jotto.jpg"),
+    name: "José Dolina",
+    mens: "Che hijo de puta, más vale que me devuelvas la guita",
+    time: "20 min."
+  },
+  {
+    key: 1,
+    image: require("./assets/avatar/seba.jpg"),
+    name: "Sebastián Serrano",
+    mens: "Eu, los chicos de IDENTIKIT son clave.",
+    time: "4 hs."
+  },
+  {
+    key: 2,
+    image: require("./assets/avatar/roca.jpg"),
+    name: "Gonzalo Waisman",
+    mens: "No sabés lo mortal que está mi nuevo plan de negocios",
+    time: "8 hs."
+  },
+  {
+    key: 3,
+    image: require("./assets/avatar/jajas.jpg"),
+    name: "Máximo Sánchez",
+    mens: "Vivimos en una sociedad.",
+    time: "12 hs."
+  },
+  {
+    key: 4,
+    image: require("./assets/avatar/nig.jpg"),
+    name: "Santino Palmieri",
+    mens: "Tenés la bandera mexicana? *ruido raro*",
+    time: "14 hs."
+  }
 ];
 
 export default class MensScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+    showIcon: true
+  };
 
-    static navigationOptions = {
-        header: null,
-        showIcon: true
-      };
-
-      renderItem = ({ item, index }) => {
-        if (item.empty === true) {
-          return <View />;
-        }
-        return (
-          <View>
-            <TouchableOpacity>
-                <CardMens
-                    key={index}
-                    mensAvatar={item.avatar}
-                    mensName={item.name}
-                    mensCont={item.mens}
-                    mensTime={item.time}
-                >
-                </CardMens>
-            </TouchableOpacity>
-          </View>
-        );
-      };
-
-
-    render() {
-        return(
-            <Container>
-                <SearchBox
-                searchPlaceholder="Buscar en Mensajes"
-                ></SearchBox>
-                <AllCont>
-                    <MensTitle>
-                        <MensText>Mensajes</MensText>
-                    </MensTitle>
-                    <FlatList
-                    data={datamens}
-                    style={styles.container}
-                    renderItem={this.renderItem}
-                    >
-                    </FlatList>
-                </AllCont>
-            </Container>
-        );
+  renderItem = ({ item, index }) => {
+    if (item.empty === true) {
+      return <View />;
     }
+    return (
+      <View style={{ width: "100%" }}>
+        <TouchableOpacity>
+          <CardMens
+            key={index}
+            mensAvatar={item.image}
+            mensName={item.name}
+            mensCont={item.mens}
+            mensTime={item.time}
+          ></CardMens>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <Container>
+        <SearchBox searchPlaceholder="Buscar en Mensajes"></SearchBox>
+        <AllCont>
+          <MensTitle>
+            <MensText>Mensajes</MensText>
+          </MensTitle>
+          <FlatList
+            data={datamens}
+            style={styles.container}
+            renderItem={this.renderItem}
+            contentContainerStyle={{ width: 320, paddingBottom: 45 }}
+          ></FlatList>
+        </AllCont>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {},
-    separator: {
-      borderBottomColor: '#bbb',
-      borderBottomWidth: StyleSheet.hairlineWidth,
-    },
-  });
+  container: {}
+  /*separator: {
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth
+  }*/
+});
 
 const Container = styled.View`
   flex: 1;
--- components/CardMens.js
+++ components/CardMens.js
@@ -15,57 +15,54 @@ import styled from "styled-components";
 import Icon from "react-native-vector-icons/Ionicons";
 
 const CardMens = props => (
-    <Container>
-        <AvatarC source={props.mensAvatar}></AvatarC>
-        <ContText>
-            <Name>{props.mensName}</Name>
-            <MensCont
-                numberOfLines={2}
-            >{props.mensCont}</MensCont>
-        </ContText>
-        <MensTime>{props.mensTime}</MensTime>
-    </Container>
+  <Container>
+    <AvatarC source={props.mensAvatar}></AvatarC>
+    <ContText>
+      <Name>{props.mensName}</Name>
+      <MensCont numberOfLines={2}>{props.mensCont}</MensCont>
+    </ContText>
+    <MensTime>{props.mensTime}</MensTime>
+  </Container>
 );
 
 export default CardMens;
 
 const Container = styled.View`
-  width: 300px;
+  width: 100%;
   height: 100px;
   align-items: center;
   justify-content: center;
   flex-direction: row;
-  background-color: white;
 `;
 
 const AvatarC = styled.Image`
-    height: 80px;
-    width: 80px;
-    border-radius: 80;
-    left: 20%;
+  height: 80px;
+  width: 80px;
+  border-radius: 80;
+  left: 20%;
 `;
 
 const ContText = styled.View`
-    height: 80%;
-    flex: 1;
-    left: 30%;
+  height: 80%;
+  flex: 1;
+  left: 30%;
 `;
 
 const Name = styled.Text`
-    font-weight: bold;
-    font-size: 20px;
-    color: #353536
+  font-weight: bold;
+  font-size: 20px;
+  color: #353536;
 `;
 
 const MensCont = styled.Text`
-    color: #9d9d9d
-    position: relative;
-    top: 5%;
-    width: 70%;
-    flex:1;
+  color: #9d9d9d;
+  position: relative;
+  top: 5%;
+  width: 70%;
+  flex: 1;
 `;
 
 const MensTime = styled.Text`
-    color: #9d9d9d;
-    right: 20%;
+  color: #9d9d9d;
+  right: 20%;
 `;
